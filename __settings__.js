2c7c9dad1155fb1914ffbe0c6b9f1018e5ccdc7b
Renz Russell Acomular <renz@sixtrees.com.sg>
2023-08-29 11:57:15 +0800

    Self-host

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1824460.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 120264034, 119545683, 119545684, 119570106, 119715077, 119715078, 119715079, 119715080, 119754941, 120139328, 120139579, 120247211, 120247212, 120255491, 120255492, 120259038, 120267698, 120774682, 120774687, 120774688, 120774689, 120774690, 147321279 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/119713919/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/119713920/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/119713918/1/ammo.js', 'preload' : true},
+];
---
+++ files/assets/120161151/1/virtual_townhall.js
@@ -0,0 +1,18 @@
+var VirtualTownhall = pc.createScript('virtualTownhall');
+
+// initialize code called once per entity
+VirtualTownhall.prototype.initialize = function() {
+
+};
+
+// update code called every frame
+VirtualTownhall.prototype.update = function(dt) {
+
+};
+
+// swap method called for script hot-reloading
+// inherit your script state here
+// VirtualTownhall.prototype.swap = function(old) { };
+
+// to learn more about script anatomy, please read:
+// https://developer.playcanvas.com/en/user-manual/scripting/
